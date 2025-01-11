09daeb34f03ba875c034df1a650c9b914d3eadb1
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-01-12 00:33:06 +0800

    Add Home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState();
+
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "1994" },
+    { id: 2, title: "Inception", release_date: "2010" },
+    { id: 3, title: "Pulp Fiction", release_date: "1994" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Searh for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
