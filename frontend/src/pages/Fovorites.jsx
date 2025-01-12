9ec6fffac87d6f6e651f6ba1c2646c4f5ec7aaca
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-01-12 09:22:41 +0800

    add Favorites components

--- frontend/src/pages/Fovorites.jsx
+++ frontend/src/pages/Fovorites.jsx
@@ -1,5 +1,10 @@
 const Fovorites = () => {
-  return <div>Fovorites</div>;
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
+    </div>
+  );
 };
 
 export default Fovorites;
