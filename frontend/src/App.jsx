4bd1076f21d910022e0eb6f9aecf42fadc116c37
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-01-12 10:45:24 +0800

    Add css styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./css/App.css";
 import Home from "./pages/Home";
 import Favorites from "./pages/Fovorites";
 import { Routes, Route } from "react-router-dom";
--- frontend/src/pages/Fovorites.jsx
+++ frontend/src/pages/Fovorites.jsx
@@ -1,3 +1,5 @@
+import "../css/Favorites.css";
+
 const Fovorites = () => {
   return (
     <div className="favorites-empty">
