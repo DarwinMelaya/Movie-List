da88268d24231f27fd93c1de2cc05c53f5d2234b
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-01-15 23:13:28 +0800

    Add Favorites function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,20 @@ import Home from "./pages/Home";
 import Favorites from "./pages/Fovorites";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import { MovieProvider } from "./contexts/MovieContext";
 function App() {
   return (
-    <div>
-      <Navbar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
-    </div>
+    <MovieProvider>
+      <div>
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </div>
+    </MovieProvider>
   );
 }
 
